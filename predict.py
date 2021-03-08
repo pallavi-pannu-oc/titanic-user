4b4dd524d1a64e33c10aff81d30140340adbd734
dkube <dkube@oneconvergence.com>
2021-03-08 12:33:11 +0000

    changes

--- predict.py
+++ predict.py
@@ -1,22 +1,35 @@
-import os
-import joblib
-import numpy as np
+from tensorflow import keras
+from dkube.sdk import DkubeFeatureSet
 import pandas as pd
-import argparse
-from dkube.sdk import *
+import json, os
 
 model_dir = "/model"
-data_dir = "/data"
+test_fs_dir= "/test_fs"
+metadata = {
+    "outputs": [
+        {
+            "storage": "inline",
+            "type": "table",
+            "format": "csv",
+            "header": ["cv_accuracy", "cv_brier_score"],
+            "source": "0.78, 0.88",
+        }
+    ]
+}
+
+with open("/output/metrics.json", "w") as f:
+    json.dump(metadata, f)
 
 def predict():
-    dkubefs = DkubeFeatureSet()
-    test_df = dkubefs.read_features(path = data_dir)
-    testdf_tmp = test_df
-    df = testdf_tmp.drop("PassengerId", 1)
-    df = pd.DataFrame(df).fillna(df.mean())
-    model = joblib.load(os.path.join(model_dir, "model.joblib"))
-    predictions = model.predict(df)
-    output = pd.DataFrame({'PassengerId': test_df.PassengerId, 'Survived': predictions})
+    os.system ("ls -l " + test_fs_dir)
+    test_df = DkubeFeatureSet.read_features(test_fs_dir)
+    print (test_df.columns)
+    x_test= test_df.drop(["PassengerId"], 1).values
+    model = keras.models.load_model(os.path.join(model_dir, "1"))
+    y_pred = model.predict(x_test)
+    y_pred[y_pred <= 0.5] = 0
+    y_pred[y_pred > 0.5] = 1
+    output = pd.DataFrame({'PassengerId': test_df.PassengerId, 'Survived': y_pred.flatten().tolist()})
     output.to_csv("/output/prediction.csv", index=False)
     print("predictions generated.")
 
